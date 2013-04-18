7051b13b8667b8ef2913db35aeaebeada712ed1c
Ian <ian901223@gmail.com>
2013-04-18 15:58:36 +0800

    Added manager

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,9 +1,9 @@
 class Manager < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :manager_name, :manager_phone, :department_id, :admin
+  attr_accessible :email, :password, :password_confirmation, :manager_name, :manager_phone, :department_id
 
-  validates :manager_name, :manager_phone, :department_id, :admin, presence: true
+  validates :manager_name, :manager_phone, :department_id, presence: true
   
   has_many :leave_applications
   belongs_to :department
