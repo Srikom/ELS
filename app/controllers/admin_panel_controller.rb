0d53352c302b6798c7791fd6b7608e71916757c6
Yung <wyungz7@gmail.com>
2013-04-22 08:42:31 +0800

    added list of Users

--- app/controllers/admin_panel_controller.rb
+++ app/controllers/admin_panel_controller.rb
@@ -3,7 +3,15 @@ class AdminPanelController < ApplicationController
 	before_filter :authenticate_admin!
 
 	def index
+		@managers = Manager.all
+		@staffs = Staff.all
+		@managements = Management.all
+	end
 
+	def show
+		@managers = Manager.find(params[:id])
+		@staffs = Staff.find(params[:id])
+		@managements = Management.find(params[:id])
 	end
 
 end
