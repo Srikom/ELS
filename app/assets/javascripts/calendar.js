b169010d365920382577e89594890a83fbbdad67
Ian <ian901223@gmail.com>
2013-04-24 16:31:47 +0800

    changes

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
 $(function(){
 	$("#leave_application_start_date").datepicker({
 	dateFormat:"dd/mm/yy",
@@ -22,9 +23,13 @@ $("#leave_application_end_date").datepicker({
  
 
 $('submit_btn').click(function() {
-        var start = $('#user_start_date').datepicker('getDate');
-        var end   = $('#user_end_date').datepicker('getDate');
+	$("#myForm").submit();
+        var start = $('#leave_application_start_date').datepicker('getDate');
+        var end   = $('#leave_application_end_date').datepicker('getDate');
         var days   = (end - start)/1000/60/60/24;
         alert(days);
-        
+        if ((end - start) > 30)
+                confirm("Selected time period is of more than 1 month duration");
+
     });
+});
--- app/controllers/management_panel_controller.rb
+++ app/controllers/management_panel_controller.rb
@@ -6,4 +6,12 @@ class ManagementPanelController < ApplicationController
 
 	end
 
+	def destroy 
+		@managements = Management.find(params[:id])	
+ 		@managements.destroy
+
+ 		flash[:notice] = "User has been deleted"
+ 		redirect_to admin_panel_index_path
+	end
+
 end
