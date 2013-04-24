8041029d139c24127eb3ff3206d0b557bb00d041
Yung <wyungz7@gmail.com>
2013-04-24 12:15:31 +0800

    Add destroy to each controller

--- app/controllers/staff_panel_controller.rb
+++ app/controllers/staff_panel_controller.rb
@@ -32,5 +32,14 @@ end
    
 
 end
+
+def destroy
+	@staffs = Staff.find(params[:id])	
+ 	@staffs.destroy
+
+ 	flash[:notice] = "User has been deleted"
+ 	redirect_to admin_panel_index_path
+end
+
 end
 
