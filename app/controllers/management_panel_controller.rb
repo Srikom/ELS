b4c45c755c6b0d2806fe8d16beec280c8fc46661
Ian <ian901223@gmail.com>
2013-04-25 10:06:48 +0800

    Updated management_controller

--- app/controllers/management_panel_controller.rb
+++ app/controllers/management_panel_controller.rb
@@ -1,6 +1,6 @@
 class ManagementPanelController < ApplicationController
 
-	before_filter :authenticate_management!
+	before_filter :authenticate_management!, :reportManagement
 
 	def index
 		@leaveApplication = LeaveApplication.managementLeave(current_management)
@@ -35,6 +35,34 @@ class ManagementPanelController < ApplicationController
 			flash[:alert] = "Please select one of the filter status options!"
 		else
 			@reviewedApplication = LeaveApplication.searchManagement(params[:search],current_management)
+		end
+	end
+
+	def reportManagement
+		@report = Report.filterReports(params[:month],params[:year])
+		@leaveApp = LeaveApplication.all
+		@leaveApp.each do |l| 
+			LeaveApplication.leaveApplicationReportEntry(l.start_date.strftime('%B'),l.start_date.strftime('%Y'),l.id)
+		end
+	end
+
+	def showReportManagement
+		@report = Report.find(params[:id])
+		@reportContent = Report.getReportContents(params[:id])
+		@leaveApproved = Report.countStatus(params[:id],"Approved")
+		@leaveRejected = Report.countStatus(params[:id],"Rejected")
+		@leavePending = Report.countStatus(params[:id],"Pending")
+		@leaveApprovedByManager = Report.countStatus(params[:id],"Approved By Manager")
+		@leaveUnsubmitted = Report.countStatus(params[:id],"Not Submitted")
+
+		respond_to do |format|
+			format.html
+			format.pdf do
+				pdf = ReportPdf.new(@report,@reportContent,@leaveApproved,@leaveRejected,@leavePending,@leaveApprovedByManager,@leaveUnsubmitted)
+				send_data pdf.render, filename:"report_#{@report.report_name}.pdf",
+									 type:"application/pdf",
+									 disposition:"inline"
+			end
 		end
 	end
 
