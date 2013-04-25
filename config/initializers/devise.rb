1b56b089ffd4778b7485cfc8919e8ede41900f49
Ian <ian901223@gmail.com>
2013-04-25 14:49:56 +0800

    change devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -238,7 +238,5 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
 
-  config.warden do |manager|
-  manager.failure_app = CustomFailure
-  end
+ 
 end
